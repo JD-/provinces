d99e787e16197cd691175620ade96f4557c26e20
JD <jd@atelier35.ca>
2018-08-30 23:20:34 -0400

    Add french locale for Canadian provinces.

--- test/check.js
+++ test/check.js
@@ -3,10 +3,11 @@ var provinces = require('../');
 var has = require('has');
 
 test('all fields present', function (t) {
-    t.plan(provinces.length * 2);
+    t.plan(provinces.length * 3);
     provinces.forEach(function (p) {
         t.ok(has(p, 'name'));
         t.ok(has(p, 'country'));
+        t.ok(has(p, 'locale'));
     });
 });
 
@@ -15,8 +16,8 @@ test('no duplicates', function (t) {
     t.plan(provinces.length);
     provinces.forEach(function (p) {
         if (!seen[p.country]) seen[p.country] = {};
-        if (p.country === 'BO') {
-            t.ok(true, 'BO exempt from unique province names');
+        if (p.country === 'BO' || p.country === 'CA') {
+            t.ok(true, 'BO and CA exempt from unique province names');//TODO better manage locales to check for duplicates in Canada
         }
         else t.ok(
             !has(seen[p.country], p.name),
